e59ce9ee01550a38859307db90428a3719d4d562
seedgs <seedgs@qq.com>
2026-04-01 22:59:07 +0800

    1、创建key_utils.gd按键字典 2、根据你实际的按键，代码会判断是哪个人物去移动

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -2,13 +2,13 @@ class_name Player
 
 extends CharacterBody2D
 
-enum ControlScheme {cpu, P1, P2}
+enum ControlScheme {CPU, P1, P2}
 
 @export var control_scheme: ControlScheme
 
-@export var speed: float = 0
+@export var speed: float
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer 
+@onready var animation_player: AnimationPlayer = %AnimationPlayer 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,20 +16,25 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	playerMove()
+func _physics_process(_delta: float) -> void:
 
-
-func playerMove() -> void:
-	var direction = Input.get_vector("P1_left", "P1_right", "P1_up", "P1_down")
-	velocity = direction * speed
-
-
-	if velocity.length() > 0:
-		animation_player.play("run")
+	if control_scheme == ControlScheme.CPU: 
+		pass # 只有玩家按下物理键的时候， 电脑（cpu）才执行行动，否则原地不动
 	else:
-		animation_player.play("idle")
+		handle_human_movement()
 
+	set_movement_animation()
+
+	move_and_slide()
+
+	
+
+func handle_human_movement() -> void:
+	var direction = KeyUtils.get_input_vector(control_scheme)  # 当你按下物理按键后，人物会移动（移动会根据你的按键去判断是对应哪个人物！！）
+	# var direction = Input.get_vector("P1_left", "P1_right", "P1_up", "P1_down")  # 当你按下物理按键后，人物移动
+
+	velocity = direction * speed
+	
 
 	# 拓展：人物根据球来判定方向：
 	# 如果球在人物的左侧，球的方向往右移动，此时如果按下人物 “右” 方向键，人物朝向为向左，且人物向右移动（俗称的倒退）
@@ -37,7 +42,18 @@ func playerMove() -> void:
 	# 按照上述逻辑，人物无论如何移动，朝向始终向着球
 	if Input.is_action_pressed("P1_left"):
 		$PlayerSprite.flip_h = true
-	elif Input.is_action_just_pressed("P1_right"):
+	elif Input.is_action_pressed("P1_right"):
 		$PlayerSprite.flip_h = false
+	
 
-	move_and_slide()
+func set_movement_animation() -> void:
+	
+
+	if velocity.length() > 0:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
+
+	
+
+	
---
+++ utils/key_utils.gd
@@ -0,0 +1,41 @@
+extends Node
+
+# 所谓电脑自动运行，就是你预先设定好的行动，在各种（玩家）按键的状态下，通过遍历字典去实施的！！！
+
+
+
+enum Action {LEFT, RIGHT, UP, DOWN, SHOOT, PASS} # 声明控制人物的物理按键（player的控制）
+
+# 下面是创建字典，也把上面的 “创建” “赋予”物理按键 
+const ACTIONS_MAP : Dictionary = {
+	Player.ControlScheme.P1: {
+		Action.LEFT: "P1_left",
+		Action.RIGHT: "P1_right",
+		Action.UP: "P1_up",
+		Action.DOWN: "P1_down",
+		Action.SHOOT: "P1_shootTheBall",
+		Action.PASS: "P1_shoot",
+	},
+	Player.ControlScheme.P2: {
+		Action.LEFT: "P2_left",
+		Action.RIGHT: "P2_right",
+		Action.UP: "P2_up",
+		Action.DOWN: "P2_down",
+		Action.SHOOT: "P2_shootTheBall",
+		Action.PASS: "P2_shoot",
+	},
+}
+
+func get_input_vector(scheme: Player.ControlScheme) -> Vector2: # 创建人物控制的方法（当按下）
+	var map : Dictionary = ACTIONS_MAP[scheme]
+	return Input.get_vector(map[Action.LEFT], map[Action.RIGHT], map[Action.UP], map[Action.DOWN])
+
+func is_action_pressed(scheme: Player.ControlScheme, action: Action) -> bool: # 当按下 时遍历字典
+	return Input.is_action_pressed(ACTIONS_MAP[scheme][action])
+
+func is_action_just_pressed(scheme: Player.ControlScheme, action: Action) -> bool: # 当刚按下 时遍历字典
+	return Input.is_action_just_pressed(ACTIONS_MAP[scheme][action])
+
+func is_action_just_released(scheme: Player.ControlScheme, action: Action) -> bool: # 当按下后 时遍历字典
+	return Input.is_action_just_released(ACTIONS_MAP[scheme][action])
+	
