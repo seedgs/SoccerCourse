bdf5094262628724f76c4de5ce823d87414b5e23
seedgs <seedgs@qq.com>
2026-04-09 18:39:39 +0800

    修改状态机（创建人物移动状态机，并添加：人物在移动的过程中可以铲球的状态）

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -31,43 +31,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 
-	if control_scheme == ControlScheme.CPU: 
-		pass 
-	else:
-		if state == State.MOVING:
-			handle_human_movement()
-			if velocity.x != 0 and KeyUtils.is_action_just_pressed(control_scheme, KeyUtils.Action.SHOOT): # 当人物移动（velocity.x 不等于0时） 和 按下“shoot”按键时， 启动铲球动作
-				state = State.TACKLING
-				time_start_tackle = Time.get_ticks_msec()
-			set_movement_animation()
-		elif state == State.TACKLING:
-			animation_player.play("tackle")
-			if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE: # 铲球动画持续时间（经过 DURATION_TACKLE 设定的时候后变回移动状态）
-				state = State.MOVING
-
-
-	
-
 	move_and_slide()
 
 	# player_direction()
 	
-	set_heading()
-
 	flip_sprite()
 
 	
-
-
-
-
-func handle_human_movement() -> void: #人物操控
-
-	var direction = KeyUtils.get_input_vector(control_scheme)  # 当你按下物理按键后，人物会移动（移动会根据你的按键去判断是对应哪个人物！！）
-	# var direction = Input.get_vector("P1_left", "P1_right", "P1_up", "P1_down")  # 当你按下物理按键后，人物移动
-
-	velocity = direction * speed
-	
+		
 
 
 func set_movement_animation() -> void: # 人物动画状态
